5705cb6e697ccf2e1216060c74e29cb5d8769112
SamiAnttalainen <115499211+SamiAnttalainen@users.noreply.github.com>
2023-01-28 15:59:27 +0200

    Add files via upload

---
+++ L01/L01T3.py
@@ -0,0 +1,14 @@
+Luku1 = 123
+Luku2 = 456
+Summa = Luku1 + Luku2
+Erotus = Luku2 - Luku1
+Tulos = Summa * Erotus
+print(str(Luku1) + " + " + str(Luku2) + " = " + str(Summa))
+print(str(Luku2) + " - " + str(Luku1) + " = " + str(Erotus))
+print(str(Summa) + " * " + str(Erotus) + " = " + str(Tulos))
+print("( " + str(Luku1) + " + " + str(Luku2) + " ) * ( " + str(Luku2) + " - " + str(Luku1) + " ) = " + str(Tulos)) 
+Jakaja = 2
+Jakojaannos1 = Luku1 % Jakaja
+Jakojaannos2 = Luku2 % Jakaja
+print("Luvun " + str(Luku1) + " jakojäännös " + str(Jakaja) + " :lla on " + str(Jakojaannos1)) 
+print("Luvun " + str(Luku2) + " jakojäännös " + str(Jakaja) + " :lla on " + str(Jakojaannos2))
---
+++ L01/L01T5.py
@@ -0,0 +1,7 @@
+Tmaara = input("Anna tyttöjen lukumäärä: ")
+Pmaara = input("Anna poikien lukumäärä: ")
+Tyttoja = int(Tmaara)
+Poikia = int(Pmaara)
+Yhteensa = Tyttoja + Poikia
+print("Tyttöjä on " + Tmaara + " ja poikia on " + Pmaara)
+print("Yhteensä luokalla on " + str(Yhteensa) + " oppilasta.")
